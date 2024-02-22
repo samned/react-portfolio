00aaf0af44fa04a1eb8bcb0a6bbf74826f10d025
samned <samuelabebayehu@gmail.com>
2024-02-22 20:46:11 +0300

    structure created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/homepage/Hero";
+import HomeContent from "./components/homepage/HomeContent";
 
 export default function Home() {
   return (
     <main>
         <Hero/>
+        <HomeContent/>
     </main>
   );
 }
