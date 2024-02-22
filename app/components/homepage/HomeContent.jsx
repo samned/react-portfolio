ab75a531365eac1291891757a9ec33ee09a2d98b
samned <samuelabebayehu@gmail.com>
2024-02-22 20:51:04 +0300

    update apostrophe

--- app/components/homepage/HomeContent.jsx
+++ app/components/homepage/HomeContent.jsx
@@ -15,7 +15,7 @@ const HomeContent = () => {
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
               gentrify, subway tile poke farm-to-table. Franzen you probably
-              haven't heard of them man bun deep jianbing selfies heirloom prism
+              haven&apos;t heard of them man bun deep jianbing selfies heirloom prism
               food truck ugh squid celiac humblebrag.
             </p>
           </div>
